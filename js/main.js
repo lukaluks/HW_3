db3ab7ca71f448eb4c5998a0cd5f42e3894318b9
lukaluks <163586723+lukaluks@users.noreply.github.com>
2024-11-02 17:22:19 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const result = 5+5+'5';
+  console.log(result);
+  console.log(typeof result);
+
+const email = "omelanenko2023@gmail.com";
+const emailSymbol = email.includes("@");
+const emailLengh = email.length;
+  console.log(emailSymbol);
+  console.log(emailLengh)
+
+let word1 = "My";
+let word2 = "name";
+let word3 = "is";
+let fullName = "My" + "name" + "is" + "Viktor";
+  console.log(fullName);
+
+let userName = Вікторія;
+let payment = 300;
+  alert("Дякуємо Вікторія, до сплати 300 гривень"); 
